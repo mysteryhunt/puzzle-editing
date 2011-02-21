20be167b8e16637f5b9e6d94595c236f22cf139c
Chris Ball <cjb@laptop.org>
2011-02-20 19:19:38 -0500

    Local config changes for Codex hunt.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	require_once "/Library/WebServer/htmlpurifier-4.0.0/library/HTMLPurifier.auto.php";
+	require_once "/var/www/htmlpurifier-4.0.0/library/HTMLPurifier.auto.php";
 
 	ini_set('default_charset', 'UTF-8');
 	ini_set('session.gc_maxlifetime','86400');
@@ -8,9 +8,9 @@
 
 	$dev = preg_match("/\/(.*)\/writing.*/", $_SERVER["SCRIPT_NAME"], $matches);
 	if ($dev) {
-	   define("URL", "https://puzzle2011.com/" . $matches[1] . "/writing");
+	   define("URL", "http://ihtfp.us/" . $matches[1] . "/writing");
 	} else {
-	   define("URL", "https://puzzle2011.com/writing");
+	   define("URL", "http://ihtfp.us/editing");
 	}
 
 	define("SELF", "$_SERVER[PHP_SELF]");
