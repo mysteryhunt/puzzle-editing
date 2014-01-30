af5d42de9e0628c243855ea10b4de5540873e013
Kevin Chen <kchen@mit.edu>
2014-01-30 04:01:56 -0500

    Remove Alice Shrugged Google Calendar.

--- editorlist.php
+++ editorlist.php
@@ -23,7 +23,5 @@
 
         echo '</table>';
 
-	echo '<iframe src="http://www.google.com/calendar/embed?src=stormynight.org_9eiihk4r23sgbvlurmf8rn9kp4%40group.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>';
-
         foot();
 ?>
