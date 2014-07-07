b39bfebcded07c6f10830322a9824b9559f6d3ca
Eric Price <ecprice@mit.edu>
2014-07-07 16:31:24 -0700

    typo fix

--- spoiled.php
+++ spoiled.php
@@ -8,7 +8,7 @@
         $uid = isLoggedIn();
 
         // Start HTML
-        head("spoiled", "Spoiled Puzzle List);
+        head("spoiled", "Spoiled Puzzle List");
 ?>
         <h2>Puzzles you're spoiled on</h2>
         <p>(Hiding dead puzzles)</p>
