b6d3aec881455b1953fa78fc048fb0db34c32d82
Brian Chen <brian.chxn@gmail.com>
2014-12-23 10:36:38 +0800

    Display summary and editornotes in FFC tables

--- ffc.php
+++ ffc.php
@@ -12,14 +12,14 @@
 
         echo "<h2>(Final) Factchecking</h2>";
         $puzzles = getPuzzlesForFactchecker($uid);
-        displayQueue($uid, $puzzles, "notes", FALSE);
+        displayQueue($uid, $puzzles, "notes summary editornotes", FALSE);
 ?>
         <h2>Puzzles Needing Final Factchecker</h2>
         <p><strong class="impt">IMPORTANT:</strong> <b>Clicking a puzzle below will
         mark you as a factchecker. Please click judiciously and complete your factchecking duties!</b></p>
 <?php
         $puzzles = getAvailablePuzzlesToFFCForUser($uid);
-        displayQueue($uid, $puzzles, "notes", FALSE, array(), "&factcheck=1");
+        displayQueue($uid, $puzzles, "notes summary editornotes", FALSE, array(), "&factcheck=1");
 
 
         // End HTML
