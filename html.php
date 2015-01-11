f2455b538515abd0b7f3e35e6f1851a0a2b0bd34
Puzzletron <puzzletron@puzzletron-aws.randomfish.org>
2015-01-10 20:55:09 -0500

    Hunt starts at 12:17 pm

--- html.php
+++ html.php
@@ -14,7 +14,7 @@ function fullTitle() {
 }
 function head($selnav = "", $title = -1) {
     if ($title == -1) {$title = fullTitle();}
-$hunt = mktime(12, 00, 00, 1, HUNT_DOM, HUNT_YEAR);
+$hunt = mktime(12, 17, 00, 1, HUNT_DOM, HUNT_YEAR);
 $now = time();
 $timediff = abs($hunt-$now);
 $days = floor($timediff/(60 * 60 * 24));
