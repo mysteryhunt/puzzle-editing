566fd7909236471ef90591c2daa031a569c4d7d9
Michael F. Booth <mike@michaelfbooth.com>
2015-03-22 16:45:07 -0400

    Fix cron jobs by adding their config from the environment.

--- email_cronjob.php
+++ email_cronjob.php
@@ -1,5 +1,12 @@
 <?php // vim:set ts=4 sw=4 sts=4 et:
 require_once "utils.php";
+
+$env_dir = getenv('ENVDIR');
+$env_file = getenv('ENVFILE');
+if (!empty($env_dir) && !empty($env_file)) {
+    Dotenv::load($env_dir, $env_file);
+}
+
 if (isset($argv[1]) && $argv[1] === "fake") {
     sendAllEmail(FALSE);
 } else {
