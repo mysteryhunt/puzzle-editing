0bc5ced9e979093afbfa21da68b57684309b3c65
Evan Broder <evan@ebroder.net>
2017-01-20 14:59:31 -0800

    Pull year from config, instead of hard-coding

--- html.php
+++ html.php
@@ -14,7 +14,7 @@ function echoNav1($selnav, $name, $linktext, $condition) {
 }
 
 function fullTitle() {
-    return 'MH2017 puzzletron authoring server (' . (DEVMODE ? 'test/dev' : (PRACMODE ? 'practice' : 'actual mystery hunt-writing')) . ' instance)';
+    return 'MH' . HUNT_YEAR . ' puzzletron authoring server (' . (DEVMODE ? 'test/dev' : (PRACMODE ? 'practice' : 'actual mystery hunt-writing')) . ' instance)';
 }
 
 function head($selnav = "", $title = -1) {
