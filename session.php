a944e16dcdf9e0e287c9386896509af122f6985f
Evan Broder <evan@ebroder.net>
2017-08-26 18:55:34 -0400

    Disable GCing sessions since it doesn't seem to be working

--- session.php
+++ session.php
@@ -43,10 +43,10 @@ function session_handler_destroy($id) {
 }
 
 function session_handler_gc($lifetime) {
-    $sql = sprintf(
-        "DELETE FROM sessions WHERE timestamp < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL %d SECOND)",
-        mysql_real_escape_string($lifetime));
-    query_db($sql);
+    //$sql = sprintf(
+    //    "DELETE FROM sessions WHERE timestamp < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL %d SECOND)",
+    //    mysql_real_escape_string($lifetime));
+    //query_db($sql);
 }
 
 session_set_save_handler(
